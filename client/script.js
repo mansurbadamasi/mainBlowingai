462ddc758848cb3c3db1a6e47c6f0bdca05b9b32
mansurbadamasi <mansurbadamasimuhammad@gmail.com>
2023-01-19 22:04:22 +0100

    update server

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv);
 
     // fetch data from server
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://mindblowingai-r7jx.onrender.com', {
         method: 'post',
         headers: {
             'content-type': 'application/json'
